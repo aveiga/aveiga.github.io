b12602242a067206d42d0aacf2bc40b6ad955169
André Veiga <a.branco.veiga@gmail.com>
2022-05-17 23:56:50 +0100

    changing the theme color to black

--- gatsby-config.js
+++ gatsby-config.js
@@ -166,7 +166,7 @@ module.exports = {
         short_name: `andreveiga.dev`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/andreveiga-icon.jpeg`, // This path is relative to the root of the site.
       },
